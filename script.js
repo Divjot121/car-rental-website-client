db39cbf5f14f49011170a6dd652f7179d8e175f2
Divjot Singh <aroradivjotsingh@gmail.com>
2024-03-13 18:27:07 +0530

    pwa

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+if ("serviceWorker" in navigator){
+    navigator.serviceWorker.register("service-worker.js").then(registration => {
+        console.log("service worker registered");
+        console.log(registration);
+    }).catch(error => {
+        console.log("service worker not registered");
+        console.log(error);
+    })
+}
 window.addEventListener('scroll', () => {
     document.querySelector('.nl-container').classList.toggle("scrolling", scrolly > 40)
 })
@@ -17,17 +26,17 @@ const sr = ScrollReveal({
     reset: true,
 })
 
-sr.reveal('.nav-top', {delay: 222, origin: 'top'})
-sr.reveal('.home .info', {delay: 444, origin: 'bottom'})
-sr.reveal('.info .booking-form', {delay: 999, origin: 'right'})
-sr.reveal('.works', {delay: 600, origin: 'top'})
-sr.reveal('.about-container .image', {delay: 322, origin: 'left'})
-sr.reveal('.about-container .content', {delay: 322, origin: 'right'})
-sr.reveal('.services .title', {delay: 322, origin: 'top' })
-sr.reveal('.services-container', {delay: 322, origin: 'bottom'})
-sr.reveal('.cars-container', {delay: 800, origin: 'bottom' })
-sr.reveal('.blog-container', {delay: 322, origin: 'left'})
-sr.reveal('.poster', {delay: 322, origin: 'bottom' })
-sr.reveal('.contact-aside', {delay: 322, origin: 'bottom'})
-sr.reveal('.contact-form', {delay: 600, origin: 'right'})
-sr.reveal('footer', {delay: 600, origin: 'top'})
+sr.reveal('.nav-top', { delay: 222, origin: 'top' })
+sr.reveal('.home .info', { delay: 444, origin: 'bottom' })
+sr.reveal('.info .booking-form', { delay: 999, origin: 'right' })
+sr.reveal('.works', { delay: 600, origin: 'top' })
+sr.reveal('.about-container .image', { delay: 322, origin: 'left' })
+sr.reveal('.about-container .content', { delay: 322, origin: 'right' })
+sr.reveal('.services .title', { delay: 322, origin: 'top' })
+sr.reveal('.services-container', { delay: 322, origin: 'bottom' })
+sr.reveal('.cars-container', { delay: 800, origin: 'bottom' })
+sr.reveal('.blog-container', { delay: 322, origin: 'left' })
+sr.reveal('.poster', { delay: 322, origin: 'bottom' })
+sr.reveal('.contact-aside', { delay: 322, origin: 'bottom' })
+sr.reveal('.contact-form', { delay: 600, origin: 'right' })
+sr.reveal('footer', { delay: 600, origin: 'top' })
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,22 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "./style.css", "./icons/apple-icon-180.png"])
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+})
+
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js")
 
 workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
+    ({ request }) => request.destination === 'image',
     new workbox.strategies.NetworkFirst()
 )
